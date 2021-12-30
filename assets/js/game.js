172725da5c1d1fcb19603aaec67aabd0379e8423
Chris Waters <floridarealty001@gmail.com>
2021-12-30 14:18:04 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,16 +6,15 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 //log multiple values at once
-console.log(playerName, playerAttack, playerHealth, playerMoney);
+console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     window.alert("Welcome to Robot Gladiators!");
-};
-fight();
+
 
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -60,39 +59,21 @@ if (playerHealth <= 0) {
   else {
     fight();
   }
+  // if player did not chose 1 or 2 in prompt
+} else {
+    window.alert("you need to pick a valid option. Try again!");
+}
+};
+
+for(var i = 0; i < enemyNames.length; i++){
+    fight(enemyNames[i]);
 }
 
 
+// Game states
 
-/*
-// subtract the value of playerAttack from the value of enemyHealth and use that result to update the value in the enemyHealth var
-enemyHealth = enemyHealth - playerAttack;
+// Win - Player robot has defeated all enemy robots
+// * fight all enemy-robots
+// * defeat each enemy robot
 
-//Log a resulting message to the console so we know that it worked.
-console.log(
-    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-);
-
-//Check enemy's health
-if (enemyHealth <= 0) {
-    window.alert(enemyName + " has died!");
-}
-else {
-    window.alert(enemyName + " still has " + enemyHealth + " health left");
-}
-
- //Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-playerHealth = playerHealth - enemyAttack;
-
- //Log a resulting message to the console so we know that it worked.
-console.log(
-    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-);
-
-// check player's health
-if (playerHealth <= 0) {
-    window.alert(playerName + " has died!");
-}
-else {
-    window.alert(playerName + " still has " + playerHealth + " health left.");
-} */
+//lose - player robot's health is zero or less
